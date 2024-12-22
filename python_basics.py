620d7bd4f418db7507fe41c1cb2cf11b0da8037c
franc menz <136167081+Francmenz@users.noreply.github.com>
2024-12-22 12:32:15 +0000

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,28 @@
+# python programming fundamentals
+
+# how to print hello world on the terminal
+print("Hello world!")
+
+# show your name on the terminal
+print("my name is Franc Menz!")
+
+# VARIABLES
+# variables are just a container for storing data 
+# example
+name = "Franc"
+# "name" is the variable of which the data "franc" has been saved.. so where ever we want to access the data called "franc", we can call the variable "name"
+
+# example
+print(name)
+# this will show "franc" on the terminal
+
+# let's make it more interesting
+# we want to greet "franc"
+print("Hello!!.. Good afternoon ", name)
+# this one will just access the container "name" and fetch the data in that container "name", so that's "franc" then greet him.....
+
+# another variable
+age = 20
+# so variable "age" stores a numerical data "20"
+print("Hello",name, " you are ", age,"years old.")
+
